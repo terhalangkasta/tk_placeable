817ab6fab8df12d7a71ffa4883f1b69d22185065
TERHALANGKASTA <136704151+terhalangkasta@users.noreply.github.com>
2026-05-24 23:14:06 +0700

    chore: bump version to 2.0.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,12 @@
 fx_version 'cerulean'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 description 'tk_placeable'
-version '1.5.0'
+version '2.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
-    '@rsg-core/shared/locale.lua',
-    'locales/en.lua',
-    'locales/*.lua',
-    'shared/config.lua',
 }
 
 client_scripts {
@@ -18,8 +14,14 @@ client_scripts {
 }
 
 server_scripts {
-    '@oxmysql/lib/MySQL.lua',
     'server/main.lua',
+}
+
+files {
+    'config/client.lua',
+    'config/server.lua',
+    'config/shared.lua',
+    'locales/*.json',
 }
 
 dependencies {
@@ -29,3 +31,4 @@ dependencies {
 }
 
 lua54 'yes'
+ox_lib 'locale'
